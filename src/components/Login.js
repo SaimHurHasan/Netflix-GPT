f867bd1ef62954f452a1cc0b25113a2988e383ec
SaimHurHasan <hasansaim115@gmail.com>
2025-02-08 01:54:34 +0000

    TMDB API data Fetch && built MainContainer

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,16 @@ import { useRef, useState } from "react";
 import Header from "./Header"
 import {checkValidData} from "../utils/validate"
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
-import {auth} from "../utils/firebase"
-import { useNavigate } from "react-router-dom"; 
+import {auth} from "../utils/firebase"; 
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 
 const Login = () => {
  const [isSignInForm, setIsSignInForm] = useState(true);
  const [errorMessage, setErrorMessage] = useState(null);
-const navigate = useNavigate();
+
 const dispatch = useDispatch();
 
 
@@ -37,7 +37,7 @@ if(!isSignInForm){
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value, 
-            photoURL: "https://avatars.githubusercontent.com/u/54436741?v=4",
+            photoURL: USER_AVATAR,
           }).then(() => {
              const {uid, email, displayName, photoURL} = auth.currentUser;
                       dispatch(
@@ -48,7 +48,6 @@ if(!isSignInForm){
                           photoURL: photoURL,
                         })
                       );
-            navigate("/browse");
           }).catch((error) => {
             setErrorMessage(error.message);
           });
@@ -67,8 +66,7 @@ if(!isSignInForm){
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
-    navigate("/browse");
+    
   })
   .catch((error) => {
     const errorCode = error.code;
